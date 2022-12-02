7ecdf3a1f45304d965868a7977b343dde0b4f4a8
Enrique6007ito@gmail.com <chinitoguerito@gmail.com>
2022-12-02 08:28:42 -0600

    pancakes

--- minesweeper.js
+++ minesweeper.js
@@ -1,11 +1,12 @@
-
+var element = document.createElement('outline');
+element.style.width = "100px";
 
 document.addEventListener('DOMContentLoaded', () => {
   const grid = document.querySelector('.grid')
   const flagsLeft = document.querySelector('#flags-left')
   const result = document.querySelector('#result')
-  let width = 10
-  let bombAmount = 20
+  let width = 8
+  let bombAmount = 40
   let flags = 0
   let squares = []
   let isGameOver = false
@@ -23,7 +24,7 @@ document.addEventListener('DOMContentLoaded', () => {
     for(let i = 0; i < width*width; i++) {
       const square = document.createElement('div')
       square.setAttribute('id', i)
-      
+
       square.classList.add(shuffledArray[i])
       grid.appendChild(square)
       squares.push(square)
